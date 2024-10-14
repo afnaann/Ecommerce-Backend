fe03748e193dc681dd7add4f8e8ea20c437307e7
afnaann <afnanpk8@gmail.com>
2024-10-14 22:33:20 +0530

    logic for products

--- products/admin.py
+++ products/admin.py
@@ -4,3 +4,4 @@ from .models import Products, Category
 
 admin.site.register(Products)
 admin.site.register(Category)
+
--- products/serializers.py
+++ products/serializers.py
@@ -1,10 +1,35 @@
+import base64
 from rest_framework import serializers
 
-from .models import Products
+from .models import Products,Category
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(write_only=True,required=False)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     class Meta:
         model = Products
-        fields = ['name','ImageLink','ImageAlt','price','color','category']
+        fields = ['name','image','Image_base64','stock','price','category']
+        extra_kwargs = {
+            'Image_base64':{'read_only':True}
+        }
     
+    def create(self, validated_data):
+        image = validated_data.pop('image',None)
+        if image:
+            Image_base64 = base64.b64encode(image.read()).decode('utf-8')
+            validated_data['Image_base64'] = Image_base64
+        
+        return Products.objects.create(**validated_data)
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','name']
+
+class ProductViewSerializer(serializers.ModelSerializer):    
+    category = CategorySerializer()
+    class Meta:
+        model = Products
+        fields = ['name','Image_base64','stock','price','category','id']
+
