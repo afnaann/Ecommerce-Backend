72c8b7c72198c94b52a164472503e9361bcba0b6
afnaann <afnanpk8@gmail.com>
2024-10-14 22:33:47 +0530

    routing setup

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get/',views.GetProducts.as_view()),
-
+    path('get/',views.ProductView.as_view()),
+    path('get/<int:id>/',views.ProductView.as_view()),
+    path('post/',views.ProductView.as_view()),
+    path('delete/<int:pk>/',views.ProductView.as_view()),
+    
+    path('category/get/',views.CategoryView.as_view()),
 ]
