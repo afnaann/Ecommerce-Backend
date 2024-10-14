5bf46ce615cdd18ba7ef8d5cbb4f19e170299f41
afnaann <afnanpk8@gmail.com>
2024-10-14 22:28:46 +0530

    db for Products

--- products/models.py
+++ products/models.py
@@ -1,21 +1,29 @@
 from django.db import models
-
+import base64
 # Create your models here.
     
 class Category(models.Model):
-    type = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
     
     def __str__(self):
-        return self.type
+        return self.name
     
 
 class Products(models.Model):
     name = models.CharField(max_length=100)
-    ImageLink = models.URLField(max_length=500)
-    ImageAlt = models.CharField(max_length=50)
+    Image_base64 = models.TextField(max_length=500,null=True)
+    stock = models.IntegerField(null=True)
     price = models.IntegerField()
-    color = models.CharField(max_length=50)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,null=True)
+
+    
+    def set_image(self, image):
+        with open(image,"rb") as image_file:
+            self.Image_base64 = base64.b64encode(image_file.read().decode('utf-8'))
+    def get_image(self):
+        return base64.b64decode(self.Image_base64)
     
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,14 +1,49 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import ProductSerializer
+from .serializers import CategorySerializer, ProductSerializer,ProductViewSerializer
 from rest_framework.permissions import IsAuthenticated
-from .models import Products
+from .models import Products,Category
+from rest_framework import status
 # Create your views here.
 
 
-class GetProducts(APIView):
+class ProductView(APIView):
+    def get(self,request,id=None):
+        if id is not None:
+            try:
+                product = Products.objects.get(id=id)
+                serializer = ProductViewSerializer(product)
+                return Response(serializer.data)
+            except Products.DoesNotExist:
+                return Response({'Error':'Product Not Found!'})
+        else:
+            products = Products.objects.all()
+            serializer = ProductViewSerializer(products,many=True)
+            return Response(serializer.data)
+    
+    # Tried to handle here, but here, the data is being taken from request.data
+    
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request, pk):
+        try:
+            product = Products.objects.get(id=pk)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Products.DoesNotExist:
+            return Response({'error':'Product Not Found!'},status=status.HTTP_404_NOT_FOUND)
+        
+    # def patch(self, request, pk):
+        
+        
+class CategoryView(APIView):
     def get(self,request):
-        products = Products.objects.all()
-        serializer = ProductSerializer(products,many=True)
+        category = Category.objects.all()
+        serializer = CategorySerializer(category,many=True)
         return Response(serializer.data)
