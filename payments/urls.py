f8a3e2705259270a1ace84975f45d881ce4cbcba
afnaann <afnanpk8@gmail.com>
2024-10-23 15:01:22 +0530

    Implemented Payment

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import StripeCheckoutView,ProcessOrderView
+from .views import StripeCheckoutView, ProcessOrderView
 
 urlpatterns = [
-    path('checkoutsession/',StripeCheckoutView.as_view()),
-    path('processorder/',ProcessOrderView.as_view()),
+    path("checkoutsession/", StripeCheckoutView.as_view()),
+    path("processorder/", ProcessOrderView.as_view()),
 ]
--- payments/views.py
+++ payments/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from django.shortcuts import redirect
 from django.http import JsonResponse
 from django.db import transaction
+from products.models import Products
 import stripe
 
 from cart.models import Cart, CartItem
@@ -13,79 +14,81 @@ from orders.models import Order, OrderItems
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+
 class StripeCheckoutView(APIView):
-    def post(self,request):
+    def post(self, request):
         data = request.data
-        cart = data.get('cart')
-        
+        cart = data.get("cart")
+
         line_items = []
         total_amount = 0
-                
+
         for each in cart:
-            line_items.append({
-                'price_data': {
-                    'currency':'inr',
-                    'product_data':{
-                        'name': each['product']['name']
+            line_items.append(
+                {
+                    "price_data": {
+                        "currency": "inr",
+                        "product_data": {"name": each["product"]["name"]},
+                        "unit_amount": int(each["product"]["price"] * 100),
                     },
-                    'unit_amount':int(each['product']['price'] * 100),
-                },
-                'quantity':each['quantity'],
-            })
-            total_amount += each['product']['price'] * each['quantity']
-        
+                    "quantity": each["quantity"],
+                }
+            )
+            total_amount += each["product"]["price"] * each["quantity"]
+
         try:
             session = stripe.checkout.Session.create(
-                payment_method_types=['card'],
+                payment_method_types=["card"],
                 line_items=line_items,
-                shipping_address_collection={
-                    'allowed_countries': ['IN','LK']
-                },
-                mode='payment',
-                success_url=f'http://localhost:5173/ordersuccess/?session_id={{CHECKOUT_SESSION_ID}}',
-                cancel_url='http://localhost:5173/shop',
+                shipping_address_collection={"allowed_countries": ["IN", "LK"]},
+                mode="payment",
+                success_url=f"http://localhost:5173/ordersuccess/?session_id={{CHECKOUT_SESSION_ID}}",
+                cancel_url="http://localhost:5173/shop",
             )
-            return Response({'id': session.url}, status=status.HTTP_200_OK)
+            return Response({"id": session.url}, status=status.HTTP_200_OK)
         except Exception as e:
-            print(e)
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-            
-            
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class ProcessOrderView(APIView):
     def get(self, request):
-        session_id = request.GET.get('session_id')
-        print('session',session_id)
+        session_id = request.GET.get("session_id")
         if not session_id:
-            return Response({'error':'Missing Session Id'},status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"error": "Missing Session Id"}, status=status.HTTP_404_NOT_FOUND
+            )
         try:
             session = stripe.checkout.Session.retrieve(session_id)
-            print(session)
             shipping_address = session.shipping_details.address
-            print(shipping_address)
-            print(request.user.id)
-            
             user = request.user
-            
             cart = Cart.objects.get(user=user)
-            
             cartItems = CartItem.objects.filter(cart=cart)
-            
-            
-            print(cart)
-            with transaction.atomic():  
+
+            with transaction.atomic():
                 order = Order.objects.create(
-                    user = request.user,
+                    user=request.user,
                     address=f"{shipping_address['line1']}, {shipping_address['city']}, {shipping_address['postal_code']}, {shipping_address['country']}",
-                    total_price = session.amount_total / 100
+                    total_price=session.amount_total / 100,
                 )
-                print(order)
-                
+
                 for item in cartItems:
                     product_id = item.product.id
                     quantity = item.quantity
-                    OrderItems.objects.create(order=order,product_id=product_id,quantity=quantity)
+                    OrderItems.objects.create(
+                        order=order, product_id=product_id, quantity=quantity
+                    )
+                    product = Products.objects.select_for_update().get(id=product_id)
 
-            return Response({'status':'Order Created Successfully!'},status=status.HTTP_201_CREATED)
+                    if product.stock >= quantity:
+                        product.stock -= quantity
+                        product.save()
+                    else:
+                        raise ValueError(f"Not enough stock for product {product.name}")
+                cart.delete()
+
+            return Response(
+                {"status": "Order Created Successfully!"},
+                status=status.HTTP_201_CREATED,
+            )
         except Exception as e:
-            print('errrrr',e)
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
